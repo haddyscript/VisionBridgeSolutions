8d1b48f1355b5d023feea57f1ef9cb8250f4e414
hadrianfdc <162952181+hadrianfdc@users.noreply.github.com>
2026-06-29 17:44:00 +0800

    Remove white overlay from Our Team/Services parallax photo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -474,7 +474,6 @@ $bridgeCableDivider = '<svg viewBox="0 0 800 60" preserveAspectRatio="none" widt
      fixed, like the site's own footer) while the page content scrolls
      past it. --}}
 <div class="relative" style="height:380px;overflow:hidden;background-image:url('@assetv('image/parallax-bg3.jpg')');background-attachment:fixed;background-size:cover;background-position:center 40%;" aria-hidden="true">
-    <div class="absolute inset-0" style="background:linear-gradient(to bottom, rgba(255,255,255,0.55) 0%, rgba(255,255,255,0.15) 50%, rgba(255,255,255,0.55) 100%);"></div>
     <div class="relative h-full flex items-center justify-center">
         <div class="bridge-cable-divider">{!! $bridgeCableDivider !!}</div>
     </div>
